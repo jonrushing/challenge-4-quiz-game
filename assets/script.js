2c8259365e6b8c1925a9035bdb1ac96c67c139a3
Jon <Jon@Minettes-MacBook-Pro.local>
2022-09-28 14:25:02 -0400

    changed a question tried to set a click event to get wrong answer. i'm burnt out

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,15 @@ const rules = document.getElementById("rulesCard")
 const quizBox = document.getElementById("quizCard")
 const questionEl= document.getElementById("question")
 const answerButtonEl = document.getElementById("answerButtons")
+const gameover = document.getElementById("gameOver")
 
 startButton.addEventListener("click", showrules)
 
+//displays the rules of the game
 function showrules(){
-    mainMenu.classList.add("hide")
+    if (mainMenu.style.display !== "none") {
+        mainMenu.style.display = "none";
+            }
     if (rules.style.display !== "grid") {
     rules.style.display = "grid";
         }
@@ -18,9 +22,15 @@ const playGame = document.getElementById("startGame")
 
 playGame.addEventListener("click", startGame)
 
+var timer = document.getElementById("timer")
+var timerStart = 31
+
+
+    
+
 var randomquestion, currentQuestionIndex
 
-
+//Main game function 
 function startGame() {
     if (rules.style.display !== "none") {
         rules.style.display = "none";
@@ -28,16 +38,30 @@ function startGame() {
     if (quizBox.style.display !== "grid") {
         quizBox.style.display = "grid";
             }
+    startTimer()
     randomquestion = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     populatequestion()
 }
-
+//Timer function
+function startTimer(){
+   const countdown = setInterval(()=>{
+        timerStart--;
+        timer.innerHTML = timerStart
+        if(timerStart <= 0){
+            clearInterval(countdown),
+            quizBox.style.display = "none",
+            gameover.style.display = "flex"
+        }
+     },1000)
+}
+//funtion used to populate the question form
 function populatequestion(){
     clearForm()
     showQuestion(randomquestion[currentQuestionIndex])
 }
 
+// makes buttons and places answers from q/a object into made buttons  
 function showQuestion(question){
     questionEl.innerText = question.question
     question.answers.forEach(answer => {
@@ -47,7 +71,7 @@ function showQuestion(question){
         if (answer.correct){
             button.dataset.correct = answer.correct
         }
-        button.addEventListener("click", pullAnswer)
+        button.addEventListener("click", submitAnswer)
         answerButtonEl.appendChild(button)
     })
 }
@@ -59,8 +83,29 @@ function clearForm(){
     }
 }
 
-function pullAnswer(event){
+function submitAnswer(event){
+    const clickedButton = event.target
+    const correct = clickedButton.dataset.correct
+    Array.from(answerButtonEl.children).forEach(button => {
+        selectionResult(button, button.dataset.correct)
+    })
+    console.log(correct)
+}
+function selectionResult(correct){
+    if (correct) { 
+        rightAnswer()
+} else {
+        wrongAnswer()
+}
 
+function rightAnswer(){
+    questionEl.innerHTML = "Correct!"
+
+function wrongAnswer(){
+    questionEl.innerHTML = "Wrong!"
+}
+
+}
 }
 const questions = [
     {question: "What sound does a cow make?",
@@ -71,12 +116,12 @@ const questions = [
         {text: "oink", correct: false}
         ]
     },
-    {question: "What sound does my ass make?",
+    {question: "What sound does the dog make?",
         answers:[
-        {text: "burp", correct: false},
-        {text: "ah-choo", correct: false},
-        {text: "fart", correct: true},
-        {text: "cough", correct: false}
+        {text: "chirp", correct: false},
+        {text: "I'm walk'n here!", correct: false},
+        {text: "bark", correct: true},
+        {text: "cluck", correct: false}
         ]
     },
     {question: "What sound does a cat make?",
